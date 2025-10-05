3a0550b22a6e482b76f0e296ab34b1dce871c9f4
Yash <yashkhatri88540@gmail.com>
2025-10-05 19:12:11 +0530

    changed version of chakra ui

--- my-app/src/components/LanguageSelector.jsx
+++ my-app/src/components/LanguageSelector.jsx
@@ -1,38 +1,40 @@
-import { Box , Text} from '@chakra-ui/react'
-import React from 'react'
-import { Button, Menu, Portal } from "@chakra-ui/react"
-import { LANGUAGE_VERSIONS } from '../constants'
+import { Box, Button, Text } from "@chakra-ui/react";
+import React, { version } from "react";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+import { LANGUAGE_VERSIONS } from "../constants";
 
-const languages = Object.entries(LANGUAGE_VERSIONS)
- 
+const languages = Object.entries(LANGUAGE_VERSIONS);
+
 const LanguageSelector = () => {
   return (
     <Box>
-      <Text mb={2} fontSize='lg'>Language: </Text>
-      <Menu.Root>
-      <Menu.Trigger asChild>
-        <Button variant="outline" size="sm">
-          JavaScript
-        </Button>
-      </Menu.Trigger>
-      <Portal>
-        <Menu.Positioner>
-          <Menu.Content>
-            {
-                languages.map((language,version)=>(
-                    <Menu.Item  key={language}>
-                    {language}
-                    
-                    <Text   as="span" color="gray.600" fontSize="sm">{version} </Text>
-                    </Menu.Item>
-                ) )
-            }
-          </Menu.Content>
-        </Menu.Positioner>
-      </Portal>
-    </Menu.Root>
+      <Text mb={2} fontSize="lg">
+        Language:{" "}
+      </Text>
+      <Menu>
+        <MenuButton as={Button}>Javacript</MenuButton>
+        <MenuList>
+          {languages.map(([language, version]) => (
+            <MenuItem color="gray.300" key={language}>{language}
+            &nbsp;
+            <Text as="span" color ="gray.500" fontSize="sm" >
+              {version}
+            </Text>
+             </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
     </Box>
-  )
-}
+  );
+};
 
-export default LanguageSelector
+export default LanguageSelector;
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,12 +2,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from './theme';
+import theme from './theme';
+
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider value={theme}>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </StrictMode>
