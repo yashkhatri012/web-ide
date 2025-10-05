797ece6d1fd6cb41594222de93a5c4c6561f1235
Yash <yashkhatri88540@gmail.com>
2025-10-05 19:27:21 +0530

    code editor boilerplate code synced with lang

--- my-app/src/components/CodeEditor.jsx
+++ my-app/src/components/CodeEditor.jsx
@@ -1,20 +1,31 @@
 import { Box } from '@chakra-ui/react'
 import React, { useRef, useState } from 'react'
 import Editor from '@monaco-editor/react';
-Box
+import LanguageSelector from './LanguageSelector';
+import { CODE_SNIPPETS } from "../constants";
 const CodeEditor = () => {
     const editorRef = useRef()
     const [value, setValue] = useState('')
+    const [language, setLanguage] = useState('javascript')
+
     const onMount =(editor) =>{
         editorRef.current = editor;
         editor.focus();
     }
+
+    const onSelect = (language) => {
+      setLanguage(language)
+      setValue(
+        CODE_SNIPPETS[language]
+      )
+    }
   return (
     <Box>
+      <LanguageSelector language={language} onSelect={onSelect}  />
       <Editor 
       theme='vs-dark'
       height="75vh" 
-      defaultLanguage="javascript" 
+      language={language}
       defaultValue="// some comment" 
       onMount={
         onMount // when we refresh the page, it should focus on editor
--- my-app/src/components/LanguageSelector.jsx
+++ my-app/src/components/LanguageSelector.jsx
@@ -14,17 +14,20 @@ import { LANGUAGE_VERSIONS } from "../constants";
 
 const languages = Object.entries(LANGUAGE_VERSIONS);
 
-const LanguageSelector = () => {
+const LanguageSelector = ({language, onSelect}) => {
   return (
     <Box>
       <Text mb={2} fontSize="lg">
         Language:{" "}
       </Text>
       <Menu>
-        <MenuButton as={Button}>Javacript</MenuButton>
+        <MenuButton as={Button}>{language}</MenuButton>
         <MenuList>
           {languages.map(([language, version]) => (
-            <MenuItem color="gray.300" key={language}>{language}
+            <MenuItem color="gray.300"
+            onClick={()=> onSelect(language) }
+            key={language}>
+              {language}
             &nbsp;
             <Text as="span" color ="gray.500" fontSize="sm" >
               {version}
